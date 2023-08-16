5d8afe4c826b81a2842837e858840bea200ef288
h-r-wells2 <hrwells10246@gmail.com>
2023-08-16 14:41:02 +0530

    sanity.io setup

--- app/components/ThemeButton.tsx
+++ app/components/ThemeButton.tsx
@@ -5,20 +5,28 @@ import { useEffect, useState } from "react";
 
 const ThemeButton = () => {
   const { setTheme, resolvedTheme } = useTheme();
-  const [ mounted, setmounted ] = useState(false);
-  
+  const [mounted, setmounted] = useState(false);
 
   useEffect(() => {
-    setmounted(true)
+    setmounted(true);
   }, []);
 
-  if(!mounted){
+  if (!mounted) {
+    // return null;
     return null;
   }
-  
+
+  const buttonStyle: React.CSSProperties = {
+    visibility: mounted ? "visible" : "hidden",
+  };
 
   return (
-    <button onClick={()=>{setTheme(resolvedTheme==='dark'? 'light':'dark')}}>
+    <button
+      style={buttonStyle}
+      onClick={() => {
+        setTheme(resolvedTheme === "dark" ? "light" : "dark");
+      }}
+    >
       {resolvedTheme === "dark" ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -31,7 +39,7 @@ const ThemeButton = () => {
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+            d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
           />
         </svg>
       ) : (
@@ -46,7 +54,7 @@ const ThemeButton = () => {
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+            d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
           />
         </svg>
       )}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
-import Image from 'next/image'
+import { client } from "./lib/sanity"
+import { Post } from "./lib/interface"
 
-export default function Home() {
+async function getData(){
+  const query = `*[_type=="post"]`
+
+  const data = await client.fetch(query)
+
+  return data
+}
+
+export default async function Home() {
+
+  const data = await getData() as Post[]
+
   return (
-   <h1>
-    HRWells blog
-   </h1>
+   <div className="divide-y divide-gray-200 dark:divide-gray-700">
+    <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+      <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-xl sm:leading-10 md:text-6xl md:leading-14">All Posts</h1>
+    </div>
+
+   </div>
   )
 }
